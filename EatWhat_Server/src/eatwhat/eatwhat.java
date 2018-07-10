0ef6d659a85d846d1d3b5ca151627392502795c0
Suyue <harmonics41@gmail.com>
2018-07-10 17:59:30 +0800

    test

--- EatWhat_Server/src/eatwhat/eatwhat.java
+++ EatWhat_Server/src/eatwhat/eatwhat.java
@@ -19,19 +19,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class eatwhat {
-    private static Thread th_close;                //執行緒
+    private static Thread th_close;                //�銵��
     private static int serverport = 5050;
-    private static ServerSocket serverSocket;    //伺服端的Socket
+    private static ServerSocket serverSocket;    //隡箸�垢��ocket
     private static ArrayList<Socket> socketlist=new ArrayList<Socket>();
     public static void main(String[] args){
         try {
-            serverSocket = new ServerSocket(serverport);    //啟動Server開啟Port接口
-            System.out.println("Server開始執行");
-            th_close=new Thread(Judge_Close);                //賦予執行緒工作(判斷socketlist內有沒有客戶端網路斷線)
-            th_close.start();                                //讓執行緒開始執行
-            // 當Server運作中時
+            serverSocket = new ServerSocket(serverport);    //���erver���ort��
+            System.out.println("Server���銵�");
+            th_close=new Thread(Judge_Close);                //鞈虫�銵�極雿�(��socketlist�����恥�蝡舐雯頝舀蝺�)
+            th_close.start();                                //霈銵���銵�
+            // �Server���葉���
             while (!serverSocket.isClosed()) {
-                // 呼叫等待接受客戶端連接
+                // ��蝑���恥�蝡舫���
                 waitNewSocket();
             }
         } catch (Exception e) {
@@ -39,17 +39,17 @@ public class eatwhat {
             e.printStackTrace();
         }
     }
-    private static Runnable Judge_Close=new Runnable(){    //讓執行緒每兩秒判斷一次SocketList內是否有客戶端強制斷線
+    private static Runnable Judge_Close=new Runnable(){    //霈銵��蝘�銝�甈﹖ocketList�����恥�蝡臬撥��蝺�
         @Override
-        public void run() {                                //在此抓取的是關閉wifi等斷線動作 
+        public void run() {                                //�甇斗������ifi蝑蝺��� 
             // TODO Auto-generated method stub
             try {
                 while(true){
                     Thread.sleep(2000);
-                    System.out.println("目前連線數"+socketlist.size());
+                    System.out.println("�����蝺"+socketlist.size());
                     for(int i=0; i<socketlist.size();i++){ //Socket close:socketlist
-                        if(isServerClose(socketlist.get(i))) {        //當該客戶端網路斷線時,從SocketList剔除
-                        	System.out.println("關閉Socket");
+                        if(isServerClose(socketlist.get(i))) {        //�閰脣恥�蝡舐雯頝舀蝺��,敺ocketList��
+                        	System.out.println("���ocket");
                             socketlist.remove(i);
                         }    
                     }
@@ -60,28 +60,28 @@ public class eatwhat {
             }
         }
     };
-    private static Boolean isServerClose(Socket socket){    //判斷連線是否中斷
+    private static Boolean isServerClose(Socket socket){    //����蝺�銝剜
         try{  
-            socket.sendUrgentData(0);        //發送一個字節的緊急數據,默認情況下是沒有開啟緊急數據處理,不影響正常連線
-            return false;                    //如正常則回傳false
+            socket.sendUrgentData(0);        //����������交���,暺����瘝�����交�����,銝蔣�甇�撣賊��蝺�
+            return false;                    //憒迤撣詨��false
         }catch(Exception e){
-            return true;                     //如連線中斷則回傳true
+            return true;                     //憒��蝺葉����true
         }  
     }  
-    // 等待接受客戶端連接
+    // 蝑���恥�蝡舫���
     public static void waitNewSocket() {
         try {
             Socket socket = serverSocket.accept();
-            System.out.println("連線成功");
-            // 呼叫創造新的使用者
+            System.out.println("��蝺���");
+            // ������蝙���
             createNewThread(socket);
         } catch (Exception e) {
             e.printStackTrace();
         }
     } 
-    // 創造新的使用者
+    // ����蝙���
     public static void createNewThread(final Socket socket) {
-        // 以新的執行緒來執行
+        // 隞交��銵��銵�
         Thread t = new Thread(new Runnable() {
         	BufferedWriter bw;
 			//BufferedWriter bw;
@@ -90,22 +90,22 @@ public class eatwhat {
         	JSONObject json_read,json_write;
         	DB db;
             @Override
-            public void run() { //Server剛啟動 App端要不到資料!
+            public void run() { //Server����� App蝡航��鞈��!
                 try {
-                    // 增加新的使用者
+                    // 憓���蝙���
                     socketlist.add(socket);
-                    //取得網路輸出串流
+                    //���雯頝航撓�銝脫��
                     bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    // 取得網路輸入串流
+                    // ���雯頝航撓�銝脫��
                     br = new BufferedReader(new InputStreamReader(socket.getInputStream()));                                   
-                    // 當Socket已連接時連續執行
+                    // �Socket撌脤�������蝥銵�
                     json_write=new JSONObject();
-                    db=new DB(); //Server開太久會連不到DB!!???? 可能是之前把宣告DB放在下面 一直宣告的關係
-                	send("sys", "連線成功");
+                    db=new DB(); //Server��云銋���銝DB!!???? ���銋��恐��B��銝 銝��摰�������
+                	send("sys", "��蝺���");
                     while (socket.isConnected()) { 
                     	String x=receive("action");
                     	System.out.println(x);
-                    	if(x.equals("show")) {	//select當指令集合                   	    
+                    	if(x.equals("show")) {	//select���誘����                   	    
                     	    /*String[] n= {"Sid", "Sname"};
                     	    Boolean[] y= {false, true};
                     	    bw.write(db.SelectTable("Select Sid, Sname from Store", n, y)+"\n");
@@ -141,14 +141,14 @@ public class eatwhat {
                     		json_write=new JSONObject();
                     		if(db.SelectNum()==0) {                  			
                     			json_write.put("Checklogin", false);    
-                    			System.out.println("無此帳號");
+                    			System.out.println("�甇文董���");
                     		}else {
                     			if(password.equals(tmp.get(0).get(0))) {
                         			json_write.put("Checklogin", true);   
-                        			System.out.println("帳密正確");
+                        			System.out.println("撣喳�迤蝣�");
                     			}else {
                         			json_write.put("Checklogin", false);
-                        			System.out.println("無此密碼");
+                        			System.out.println("�甇文�Ⅳ");
                     			}
                     		}
                     		bw.write(json_write+"\n");
@@ -172,10 +172,10 @@ public class eatwhat {
                     		//System.out.println("tmp: "+tmp);
                     		if(tmp.indexOf("Account")!=-1) {
                     			json_write.put("check", false);
-                    			json_write.put("data", "這個帳號已經有人使用");
+                    			json_write.put("data", "��董��歇蝬�犖雿輻");
                     		}else if(tmp.indexOf("Mail")!=-1) {
                     			json_write.put("check", false);
-                    			json_write.put("data", "信箱已被註冊");
+                    			json_write.put("data", "靽∠拳撌脰◤閮餃��");
                     		}else {
                     			json_write.put("check", true);
                     		}
@@ -199,26 +199,26 @@ public class eatwhat {
                     		String s="";
                     		switch(typ) {
                     			case 1:
-                    				s="Having (Kind1 like \"%午餐%\" Or Kind1 like \"%晚餐%\") ";
+                    				s="Having (Kind1 like \"%����%\" Or Kind1 like \"%����%\") ";
                     				break;
                     			case 2:
-                    				s="Having Kind1 like \"%早餐%\" ";
+                    				s="Having Kind1 like \"%�擗�%\" ";
                     				break;
                     			case 3:
-                    				s="Having Kind1 like \"%點心%\" ";
+                    				s="Having Kind1 like \"%暺��%\" ";
                     				break;
                     		}
                     		for(int i=0;i<dont.length();i++) {
                     			s+="And Kind1 not like \"%"+dont.get(i).toString()+"%\" ";
                     		}
-                    		//System.out.println("自己所在經緯度: "+lat+","+lon);
-                    		//System.out.println("指定距離: "+dis);
+                    		//System.out.println("�撌望��蝬楝摨�: "+lat+","+lon);
+                    		//System.out.println("����: "+dis);
                     		for(int i=0;i<tmp.size();i++) {	 
                     			String ss=tmp.get(i).get(1);
-                    			//System.out.println("地址: "+ss);
+                    			//System.out.println("���: "+ss);
                 				double[] cal=getGPFromAddress(ss);
-                				//System.out.println("該店經緯度: "+cal[0]+","+cal[1]);
-                				//System.out.println("相距: "+Distance(cal[0], cal[1], lat, lon));
+                				//System.out.println("閰脣��楝摨�: "+cal[0]+","+cal[1]);
+                				//System.out.println("�頝�: "+Distance(cal[0], cal[1], lat, lon));
                 				if(Distance(cal[0], cal[1], lat, lon)<=dis) {
                 					int id=Integer.parseInt(tmp.get(i).get(0));     					
                 					String sql="Select Sname, Address, Mname, Price, group_concat(Kkind) as Kind1 from Store, Menu, Storemenu, Menukind, Kind Where Sid="+id+" And Mid=S_mid And Sid=Ssid And K_mid=Mid And M_kid=Kid group by Mname, Price "+s+"Order by Rand() Limit 1";
@@ -233,7 +233,7 @@ public class eatwhat {
                 					}
                 				}
                 				json_write.put("check", false);
-                				json_write.put("data", "範圍內無符合之料理");
+                				json_write.put("data", "蝭��蝚血�����");
                     		}
                     		//System.out.println(json_write.toString());
                     		bw.write(json_write+"\n");
@@ -241,22 +241,22 @@ public class eatwhat {
                     	}else if(x.equals("close")) {               	
                     		socketlist.remove(socket);
                     	}
-                    	//send("sys", "連線成功");
+                    	//send("sys", "��蝺���");
                     	//System.out.println(receive("sys"));
-                        /*tmp = br.readLine();        //宣告一個緩衝,從br串流讀取值
-                        // 如果不是空訊息
+                        /*tmp = br.readLine();        //摰�����楨銵�,敺r銝脫������
+                        // 憒��蝛箄�
                         if(tmp!=null){
-                            //將取到的String抓取{}範圍資料
+                            //撠���tring���}蝭����
                             tmp=tmp.substring(tmp.indexOf("{"), tmp.lastIndexOf("}") + 1);
                             json_read=new JSONObject(tmp);
-                            //從客戶端取得值後做拆解,可使用switch做不同動作的處理與回應
-                        }else{    //在此抓取的是使用使用強制關閉app的客戶端(會不斷傳null給server)
-                            //當socket強制關閉app時移除客戶端
+                            //敺恥�蝡臬���澆���圾,�雿輻switch�������������
+                        }else{    //�甇斗���雿輻雿輻撘瑕���pp��恥�蝡�(����null蝯存erver)
+                            //�socket撘瑕���pp��宏�摰Ｘ蝡�
                             socketlist.remove(socket);
-                            break;    //跳出迴圈結束該執行緒    
+                            break;    //頝喳餈游���府�銵��    
                         }*/
                     }
-                    System.out.println("已斷線");
+                    System.out.println("撌脫蝺�");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }    
@@ -270,14 +270,14 @@ public class eatwhat {
                 tmp=br.readLine();
                 tmp=tmp.substring(tmp.indexOf("{"), tmp.lastIndexOf("}") + 1);
                 json_read=new JSONObject(tmp);
-                return URLDecoder.decode(json_read.getString(n), "utf-8");//可不可以做成判斷式
+                return URLDecoder.decode(json_read.getString(n), "utf-8");//�銝隞亙���撘�
             }
-            //地址轉經緯
+            //���頧�楝
             public double[] getGPFromAddress(String addr) {
             	try {
         			String tmp = URLEncoder.encode(addr, "UTF-8");       			        			
         			InputStream is = new URL("http://maps.googleapis.com/maps/api/geocode/json?address="+tmp+"&language=zh-tw").openStream();
-        			BufferedReader rd = new BufferedReader(new InputStreamReader(is,"utf-8")); //避免中文亂碼問題
+        			BufferedReader rd = new BufferedReader(new InputStreamReader(is,"utf-8")); //���葉���Ⅳ����
                     StringBuilder sb = new StringBuilder();
                     int cp;
                     while ((cp = rd.read()) != -1) {
@@ -298,7 +298,7 @@ public class eatwhat {
         			return null;
         		}
             }
-            //帶入使用者及景點店家經緯度可計算出距離
+            //撣嗅雿輻��暺�振蝬楝摨血閮�頝
             public double Distance(double longitude1, double latitude1, double longitude2,double latitude2) {
                 double radLatitude1 = latitude1 * Math.PI / 180;
                 double radLatitude2 = latitude2 * Math.PI / 180;
@@ -313,7 +313,7 @@ public class eatwhat {
                 return distance ;
             }
         });
-        // 啟動執行緒
+        // ���銵��
         t.start();
     }
 }
